a1ee9c12b59e80b4e9312ead3b63072aa968166a
Ainor Syahrizal <macintorch@gmail.com>
2017-12-20 15:22:34 +0800

    draw shapes

--- core/src/com/ainorgames/floppybird/FloppyBird.java
+++ core/src/com/ainorgames/floppybird/FloppyBird.java
@@ -2,15 +2,23 @@ package com.ainorgames.floppybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Rectangle;
 
 import java.util.Random;
+
+
 
 public class FloppyBird extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture background;
+
+	ShapeRenderer shapeRenderer;
 
 	Texture[] birds;
 	int flapState = 0;
@@ -33,6 +41,10 @@ public class FloppyBird extends ApplicationAdapter {
 	float[] tubeX = new float[numberOfTubes];
 	float[] tubeOffset = new float[numberOfTubes];
 	float distanceBetweenTubes;
+
+	Circle birdCircle;
+	Rectangle[] topTubeRectangles;
+	Rectangle[] bottomTubeRectangles;
 	
 	@Override
 	public void create () {
@@ -43,11 +55,18 @@ public class FloppyBird extends ApplicationAdapter {
 		birds[0] = new Texture("bird.png");
 		birds[1] = new Texture("bird2.png");
 
+		shapeRenderer = new ShapeRenderer();
+
+		birdCircle = new Circle();
+
+
 		birdY = Gdx.graphics.getHeight() / 2 - birds[0].getWidth() /2;
 
 		topTube = new Texture("toptube.png");
 		bottomTube = new Texture("bottomtube.png");
 		maxTubeOffset = Gdx.graphics.getHeight() / 2 - gap / 2 - 100;
+		topTubeRectangles = new Rectangle[numberOfTubes];
+		bottomTubeRectangles = new Rectangle[numberOfTubes];
 
 		randomGenerator = new Random();
 
@@ -57,6 +76,9 @@ public class FloppyBird extends ApplicationAdapter {
 
 			tubeOffset[i] = (randomGenerator.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - gap - 200);
 			tubeX[i] = Gdx.graphics.getWidth() / 2 - topTube.getWidth() / 2 + i * distanceBetweenTubes;
+
+			topTubeRectangles[i] = new Rectangle();
+			bottomTubeRectangles[i] = new Rectangle();
 
 		}
 
@@ -81,6 +103,7 @@ public class FloppyBird extends ApplicationAdapter {
 				if (tubeX[i] < - topTube.getWidth()) {
 
 					tubeX[i] += numberOfTubes * distanceBetweenTubes;
+					tubeOffset[i] = (randomGenerator.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - gap - 200);
 
 				}
 				tubeX[i] = tubeX[i] - tubeVelocity;
@@ -88,6 +111,8 @@ public class FloppyBird extends ApplicationAdapter {
 				batch.draw(topTube, tubeX[i] , Gdx.graphics.getHeight() / 2 + gap / 2 + tubeOffset[i] );
 				batch.draw(bottomTube, tubeX[i], Gdx.graphics.getHeight()/ 2 - gap / 2 - bottomTube.getHeight() + tubeOffset[i]);
 
+				topTubeRectangles[i] = new Rectangle(tubeX[i], Gdx.graphics.getHeight() / 2 + gap / 2 + tubeOffset[i], topTube.getWidth(), topTube.getHeight());
+				bottomTubeRectangles[i] = new Rectangle(tubeX[i], Gdx.graphics.getHeight()/ 2 - gap / 2 - bottomTube.getHeight() + tubeOffset[i], bottomTube.getWidth(), bottomTube.getHeight());
 			}
 
 			if (birdY > 0 || velocity < 0 ) {
@@ -116,6 +141,22 @@ public class FloppyBird extends ApplicationAdapter {
 
 		batch.draw(birds[flapState],Gdx.graphics.getWidth() / 2 - birds[flapState].getWidth() / 2, birdY );
 		batch.end();
+
+		birdCircle.set(Gdx.graphics.getWidth() / 2, birdY + birds[flapState].getHeight() / 2, birds[flapState].getWidth() / 2 );
+
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer.setColor(Color.RED);
+		shapeRenderer.circle(birdCircle.x, birdCircle.y, birdCircle.radius);
+
+
+		for (int i = 0; i <numberOfTubes; i++ ) {
+			shapeRenderer.rect(tubeX[i], Gdx.graphics.getHeight() / 2 + gap / 2 + tubeOffset[i], topTube.getWidth(), topTube.getHeight());
+			shapeRenderer.rect(tubeX[i], Gdx.graphics.getHeight()/ 2 - gap / 2 - bottomTube.getHeight() + tubeOffset[i], bottomTube.getWidth(), bottomTube.getHeight());
+
+
+
+		}
+		shapeRenderer.end();
 
 	}
 	
