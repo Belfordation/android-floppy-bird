e7066645ab315e1c190e1e07c7bdbe151b3bee6a
Ainor Syahrizal <macintorch@gmail.com>
2017-12-20 10:20:15 +0800

    display background and bird

--- core/src/com/ainorgames/floppybird/FloppyBird.java
+++ core/src/com/ainorgames/floppybird/FloppyBird.java
@@ -8,22 +8,43 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FloppyBird extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture background;
+	Texture bird;
+
+	Texture[] birds;
+	int flapState = 0;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		background = new Texture("bg.png");
+		bird = new Texture("bird.png");
+
+		birds = new Texture[2];
+		birds[0] = new Texture("bird.png");
+		birds[1] = new Texture("bird2.png");
 
 	}
 
 	@Override
 	public void render () {
 
+		if (flapState == 0) {
+			flapState = 1;
+		} else {
+			flapState = 0;
+		}
+
+		batch.begin();
+		batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+		batch.draw(bird,Gdx.graphics.getWidth() / 2 - bird.getWidth() / 2, Gdx.graphics.getHeight() / 2 - bird.getWidth() /2 );
+		batch.end();
+
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
